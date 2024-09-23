0a1b01a76be210095219a5505bc758591c964a3b
MKJ332 <113014085+MKJ332@users.noreply.github.com>
2024-09-23 16:44:15 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Skrypt PHP</title>
+    <meta charset="UTF-8">
+    
+</head>
+<body>
+    <p>
+            <?php
+        $liczbaCalkowita = 12;
+        $liczbaRzeczywista = 15.67;
+        $ciagZnakow = "Witam PHP";
+
+        echo "Wartosc liczby calkowitej: $liczbaCalkowita <br>";
+        echo "Wartosc liczby rzeczywistej: $liczbaRzeczywista <br>";
+        echo "Wartosc ciagu znakow: $ciagZnakow <br>";
+
+            ?>
+        </p>
+        
+</body>
+</html>
